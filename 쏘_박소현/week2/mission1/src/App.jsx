4517a34c77dc38bce11560755fccd9773982cc95
Sohyunnnn <thgus05061@naver.com>
2024-10-02 00:42:17 +0900

    FEAT: todo 수정 기능 구현

--- 쏘_박소현/week2/mission1/src/App.jsx
+++ 쏘_박소현/week2/mission1/src/App.jsx
@@ -7,7 +7,9 @@ function App() {
 
   const [text, setText] =useState('');
 
-  console.log(text)
+  const [editingId, setEditingId] =useState('');
+
+  const [editText, setEditText]= useState('');
 
   //랜더링 방지 => form 태그 안에서 버튼을 누르면 리랜더링 되는데 그것을 방지하기 위한 것
   const handleSubmit =(e)=>{
@@ -28,7 +30,14 @@ function App() {
   const deleteTodo = (id) => {
     setTodos((prev) => prev.filter((item) => item.id !== id));
   };
-  
+
+  //수정진행
+  const updateTodo = (id, text)=> {
+    setTodos((prev) => 
+      prev.map((item)=> (item.id === id ? {...item, task:text}: item))
+    )
+    setEditingId('');
+  }
   
 
   return (
@@ -41,8 +50,28 @@ function App() {
       {todos.map((todo, _)=> (
         <>
         <div style={{display:'flex' ,gap:'20px'}}>
-          <h4 key={todo.id}>{todo.id}.{todo.task}</h4>
+          <div key={todo.id} style={{display:'flex' ,gap:'5px'}}>
+            {editingId === todo.id ? (
+          // 수정 중일 때
+          <>
+            <p>{todo.id}</p>
+            <input defaultValue={todo.task} onChange={(e)=> setEditText(e.target.value)}/>
+          </>
+           ) : (
+          // 수정 중이 아닐 때
+          <>
+            <p>{todo.id}</p>
+            <p>{todo.task}</p>
+          </>
+          )}
+            </div>
           <button onClick={()=> deleteTodo(todo.id)}>삭제하기</button>
+          {editingId===todo.id? (
+            <button onClick={()=> updateTodo(editingId,editText)} >수정 완료</button>
+          ):(
+            <button onClick={()=> setEditingId(todo.id)} >수정 진행</button>
+          )}
+          
         </div>
         </>
       ))}
