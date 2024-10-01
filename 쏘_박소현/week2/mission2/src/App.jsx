0830362c3931f60bf09fea3ded72327cae69dbcd
Sohyunnnn <thgus05061@naver.com>
2024-10-02 02:32:58 +0900

    FEAT: 영화 이미지 컴포넌트 분리

--- 쏘_박소현/week2/mission2/src/App.jsx
+++ 쏘_박소현/week2/mission2/src/App.jsx
@@ -1,43 +1,11 @@
-import { useState } from 'react';
-import { MOVIES } from './mocks/movies';
-
-const baseUrl = 'https://image.tmdb.org/t/p/w500';
+import Movie from "./components/Movie";
 
 const App = () => {
-  const [hoveredMovieId, setHoveredMovieId] = useState(null); 
 
   return (
-    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(10, 1fr)', gap: '10px' }}>
-      {MOVIES.results.map(movie => (
-        <div
-          key={movie.id}
-          onMouseEnter={() => setHoveredMovieId(movie.id)} 
-          onMouseLeave={() => setHoveredMovieId(null)} 
-          style={{ position: 'relative', overflow: 'hidden' }}
-        >
-          <img
-            src={`${baseUrl}${movie.poster_path}`}
-            alt={movie.title}
-            style={{
-              width: '100%',
-              borderRadius: '5px'
-            }}
-          />
-          {hoveredMovieId === movie.id && (
-            <div
-              style={{
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-                backgroundColor: 'rgba(0, 0, 0, 0.7)',
-              }}
-            />
-          )}
-        </div>
-      ))}
-    </div>
+    <>
+      <Movie />
+    </>
   );
 };
 
