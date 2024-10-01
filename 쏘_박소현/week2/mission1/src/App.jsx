17ae236ec35315f6dc9ec7aa22e616c3dce70645
Sohyunnnn <thgus05061@naver.com>
2024-10-02 01:08:36 +0900

    FEAT: 컴포넌트 분리 input 및 button 컴포넌트 분리 진행

--- 쏘_박소현/week2/mission1/src/App.jsx
+++ 쏘_박소현/week2/mission1/src/App.jsx
@@ -1,84 +1,81 @@
 import { useState } from "react";
-
+import Button from "./components/Button";
+import Input from "./components/Input";
 
 function App() {
+  const [todos, setTodos] = useState([{ id: 1, task: "투두 만들기" }]);
+  const [text, setText] = useState("");
+  const [editingId, setEditingId] = useState("");
+  const [editText, setEditText] = useState("");
 
-  const [todos, setTodos]= useState([{id: 1, task:'투두 만들기'}]);
-
-  const [text, setText] =useState('');
-
-  const [editingId, setEditingId] =useState('');
-
-  const [editText, setEditText]= useState('');
-
-  //랜더링 방지 => form 태그 안에서 버튼을 누르면 리랜더링 되는데 그것을 방지하기 위한 것
-  const handleSubmit =(e)=>{
+  // 랜더링 방지 => form 태그 안에서 버튼을 누르면 리랜더링 방지
+  const handleSubmit = (e) => {
     e.preventDefault();
-  }
-
-  //추가하기
-  const addTodo= ()=>{
-    if(text.trim().length ===0 ){
-      alert('텍스트를 입력해주세요.')
-    }
-    setTodos((prev)=> [
-      ...prev, 
-      {id:Math.floor(Math.random()*100 +2), task: text}
-    ]);
-    setText('');
-
   };
 
-  //삭제하기
+  // 추가하기
+  const addTodo = () => {
+    if (text.trim().length === 0) {
+      alert("텍스트를 입력해주세요.");
+      return;
+    }
+    setTodos((prev) => [...prev, { id: Math.floor(Math.random() * 100 + 2), task: text }]);
+    setText("");
+  };
+
+  // 삭제하기
   const deleteTodo = (id) => {
     setTodos((prev) => prev.filter((item) => item.id !== id));
   };
 
-  //수정진행
-  const updateTodo = (id, text)=> {
-    setTodos((prev) => 
-      prev.map((item)=> (item.id === id ? {...item, task:text}: item))
-    )
-    setEditingId('');
-  }
-  
+  // 수정 진행
+  const updateTodo = (id, text) => {
+    setTodos((prev) =>
+      prev.map((item) => (item.id === id ? { ...item, task: text } : item))
+    );
+    setEditingId("");
+  };
 
   return (
     <>
-    <form onSubmit={handleSubmit}>
-      <input type='text' value={text} onChange={(e)=> setText(e.target.value)}/>
-      <button onClick={()=> addTodo()} type="submit">할 일 등록</button>
-    </form>
-    <div>
-      {todos.map((todo)=> (
-        <div key={todo.id} style={{display:'flex' ,gap:'20px'}}>
-          <div key={todo.id} style={{display:'flex' ,gap:'5px'}}>
-            {editingId === todo.id ? (
-          // 수정 중일 때
-          <>
-            <p>{todo.id}</p>
-            <input defaultValue={todo.task} onChange={(e)=> setEditText(e.target.value)}/>
-          </>
-           ) : (
-          // 수정 중이 아닐 때
-          <>
-            <p>{todo.id}</p>
-            <p>{todo.task}</p>
-          </>
-          )}
+      <form onSubmit={handleSubmit}>
+        <Input value={text} onChange={(e) => setText(e.target.value)} />
+        <Button onClick={addTodo} type="submit">
+          할 일 등록
+        </Button>
+      </form>
+      <div>
+        {todos.map((todo) => (
+          <div key={todo.id} style={{ display: "flex", gap: "20px" }}>
+            <div style={{ display: "flex", gap: "5px" }}>
+              {editingId === todo.id ? (
+                // 수정 중일 때
+                <>
+                  <p>{todo.id}</p>
+                  <Input
+                    defaultValue={todo.task}
+                    onChange={(e) => setEditText(e.target.value)}
+                  />
+                </>
+              ) : (
+                // 수정 중이 아닐 때
+                <>
+                  <p>{todo.id}</p>
+                  <p>{todo.task}</p>
+                </>
+              )}
             </div>
-          <button onClick={()=> deleteTodo(todo.id)}>삭제하기</button>
-          {editingId===todo.id? (
-            <button onClick={()=> updateTodo(editingId,editText)} >수정 완료</button>
-          ):(
-            <button onClick={()=> setEditingId(todo.id)} >수정 진행</button>
-          )}
-          
-        </div>
-      ))}
-    </div>
+            <Button onClick={() => deleteTodo(todo.id)}>삭제하기</Button>
+            {editingId === todo.id ? (
+              <Button onClick={() => updateTodo(editingId, editText)}>수정 완료</Button>
+            ) : (
+              <Button onClick={() => setEditingId(todo.id)}>수정 진행</Button>
+            )}
+          </div>
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
