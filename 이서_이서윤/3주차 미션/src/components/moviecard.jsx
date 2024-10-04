22704ff8d4895bdd190557d2949255d3eaea6905
seoyoon lee <leeseoyoon01@gmail.com>
2024-10-04 23:21:10 +0900

    FIX: 제목 overflow 수정, hover 추가

--- 이서_이서윤/3주차 미션/src/components/moviecard.jsx
+++ 이서_이서윤/3주차 미션/src/components/moviecard.jsx
@@ -4,9 +4,19 @@ import styled from "styled-components";
 const Wrapper=styled.div`
     margin:10px;
     height:250px;
+    position:relative;
     div{
         color:white;
         margin-top:-3px;
+    }
+`;
+const Div=styled.div`
+    position:absolute;
+    top:3px;
+    width:150px;
+    height:210px;
+    &:hover{
+        background-color:rgba(0,0,0,0.5);
     }
 `;
 const Img=styled.img`
@@ -17,6 +27,10 @@ const Img=styled.img`
 const Title=styled.div`
     font-size:14px;
     font-weight:800;
+    width:150px;
+    white-space: nowrap;  
+    overflow: hidden; 
+    text-overflow: ellipsis; 
 `;
 const Date=styled.div`
     font-size:11px;
@@ -26,6 +40,7 @@ const MovieCard=({key, title, poster, release_date})=>{
     return(
         <>
             <Wrapper>
+                <Div></Div>
                 <Img src={`${src}${poster}`} alt={key}/>
                 <Title>{title}</Title>
                 <Date>{release_date}</Date>
