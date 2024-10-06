ce421d4112b5c066fdceaf6dee3e4170517c130a
Minji Kim <minjikim2000@hotmail.com>
2024-10-07 06:38:08 +0900

    feat: add router

--- 레미_김민지/3주차 미션/MoviePage/src/App.jsx
+++ 레미_김민지/3주차 미션/MoviePage/src/App.jsx
@@ -1,11 +1,18 @@
-import Movies from "./components/Movies";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>홈 페이지</h1>,
+  },
+  {
+    path: "/movies",
+    element: <h1>영화 페이지</h1>,
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <Movies />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
