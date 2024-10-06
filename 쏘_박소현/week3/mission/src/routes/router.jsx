3818f20a4942b6d57573bf079eb96a147fd86a25
Sohyunnnn <thgus05061@naver.com>
2024-10-06 22:52:39 +0900

    FEAT: 헤더 추가 및 outlet 적용

--- 쏘_박소현/week3/mission/src/routes/router.jsx
+++ 쏘_박소현/week3/mission/src/routes/router.jsx
@@ -2,19 +2,28 @@ import {createBrowserRouter} from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import NotFound from "../pages/NotFound";
 import MoviesPage from "../pages/MoviesPage";
+import RootLayout from "../layout/RootLayout";
 
 
 const router = createBrowserRouter([
   {
       path: '/',
-      element: <HomePage/>,
-      errorElement: <NotFound/>
+      element: <RootLayout/>,
+      errorElement: <NotFound/>,
+      children: [
+        {
+            index: true,
+            element: <HomePage/>
+        },
+        {
+            path: 'movies',
+            element: <MoviesPage/>
+        }
+    ]
   },
-  {
-      path: '/movies',
-      element: <MoviesPage/>
-  }
-])
+
+  ])
+
 
 
 export default router
