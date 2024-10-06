48b148224a5df12aea5ea4ff31c2cb3c4cc348e3
Sohyunnnn <thgus05061@naver.com>
2024-10-07 00:26:47 +0900

    FEAT: styled-components로 스타일 수정

--- 쏘_박소현/week3/mission/src/components/Movie.jsx
+++ 쏘_박소현/week3/mission/src/components/Movie.jsx
@@ -1,10 +1,42 @@
 import { useState, useEffect } from 'react';
 import { getMovies } from '../api/movies/getMovies';
+import styled from 'styled-components';
 
 const baseUrl = 'https://image.tmdb.org/t/p/w500';
 
+const MovieGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(10, 1fr);
+  gap: 10px;
+`;
+
+const MovieItem = styled.div`
+  position: relative;
+  overflow: hidden;
+
+  &:hover div {
+    opacity: 1;
+  }
+`;
+
+const MovieImage = styled.img`
+  width: 100%;
+  border-radius: 5px;
+`;
+
+const HoverOverlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  border-radius: 5px;
+  background-color: rgba(0, 0, 0, 0.7);
+  opacity: 0; 
+  transition: opacity 0.3s ease; 
+`;
+
 const Movie = () => {
-  const [hoveredMovieId, setHoveredMovieId] = useState(null); 
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -21,37 +53,21 @@ const Movie = () => {
   }, []);
 
   return (
-    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(10, 1fr)', gap: '10px' }}>
+    <MovieGrid>
       {movies.map(movie => (
-        <div
-          key={movie.id}
-          onMouseEnter={() => setHoveredMovieId(movie.id)} 
-          onMouseLeave={() => setHoveredMovieId(null)} 
-          style={{ position: 'relative', overflow: 'hidden' }}
-        >
-          <img
+        <div key={movie.id}>
+          <MovieItem key={movie.id}>
+          <MovieImage
             src={`${baseUrl}${movie.poster_path}`}
             alt={movie.title}
-            style={{
-              width: '100%',
-              borderRadius: '5px'
-            }}
           />
-          {hoveredMovieId === movie.id && (
-            <div
-              style={{
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-                backgroundColor: 'rgba(0, 0, 0, 0.7)',
-              }}
-            />
-          )}
+          <HoverOverlay /> 
+        </MovieItem>
+        <p>{movie.title}</p>
+        <p>{movie.release_date}</p>
         </div>
       ))}
-    </div>
+    </MovieGrid>
   );
 };
 
