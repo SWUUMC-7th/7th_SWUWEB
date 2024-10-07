854cc69e17d76be9ab55622af70e224ce48f44ee
Minji Kim <minjikim2000@hotmail.com>
2024-10-07 14:01:37 +0900

    fix: 콘텐츠가 사이드바 옆으로 오도록 수정

--- 레미_김민지/3주차 미션/MoviePage/src/layout/root-layout.jsx
+++ 레미_김민지/3주차 미션/MoviePage/src/layout/root-layout.jsx
@@ -1,15 +1,40 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar.jsx";
 import Sidebar from "../components/sidebar.jsx";
+import styled from "styled-components";
 
 const RootLayout = () => {
   return (
-    <>
+    <Container>
       <Navbar />
-      <Sidebar />
-      <Outlet />
-    </>
+      <MainContent>
+        <Sidebar />
+        <ContentArea>
+          <Outlet />
+        </ContentArea>
+      </MainContent>
+    </Container>
   );
 };
 
 export default RootLayout;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
+
+const MainContent = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+`;
+
+const ContentArea = styled.div`
+  flex: 1;
+  padding: 20px;
+  background-color: #191919;
+  color: white;
+  /* overflow-y: auto; 내용이 길어지면 스크롤 */
+`;
