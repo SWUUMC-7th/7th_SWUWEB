24ba5a70ffc535de4a39dce16d0977caf4317760
seoyoon lee <leeseoyoon01@gmail.com>
2024-10-10 21:33:49 +0900

    CHORE: API 키 .env 파일에 저장

---
+++ 이서_이서윤/4주차 미션/src/App.jsx
@@ -0,0 +1,64 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+import HomePage from "./pages/home.jsx";
+import NotFound from "./pages/not-found.jsx";
+import Movies from "./pages/movies.jsx";
+import RootLayout from './layout/root-layout.jsx';
+import LogIn from './pages/login.jsx';
+import SignUp from './pages/signup.jsx';
+import Search from "./pages/search.jsx";
+import NowPaying from "./pages/now-playing.jsx";
+import Popular from "./pages/popular.jsx";
+import TopRated from "./pages/top-rated.jsx";
+import UpComing from "./pages/up-coming.jsx";
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <RootLayout/>,
+        errorElement: <NotFound/>,
+        children:[
+          {
+            index:true, // 홈 경로
+            element:<HomePage/>
+          },
+          {
+            path:'movies', //부모 path : '/'이라 '/movies'라 쓰지 않아도 됨
+            element:<Movies/>
+          },
+          {
+            path:'login',
+            element:<LogIn/>
+          },
+          {
+            path:'signup',
+            element:<SignUp/>
+          },
+          {
+            path:'search',
+            element:<Search/>
+          },
+          {
+            path:'movies/now-playing',
+            element:<NowPaying/>
+          },
+          {
+            path:'movies/popular',
+            element:<Popular/>
+          },
+          {
+            path:'movies/top-rated',
+            element:<TopRated/>
+          },
+          {
+            path:'movies/up-coming',
+            element:<UpComing/>
+          },
+        ]
+    }
+])
+
+function App() {
+    return <RouterProvider router={router}/>
+}
+
+export default App
---
+++ 이서_이서윤/4주차 미션/src/pages/top-rated.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import MovieCard from "../components/moviecard";
+import styled from "styled-components";
+
+const Container=styled.div`
+    width: clac(100vw - 200px);
+    background-color:black;
+    display:flex;
+    flex-wrap:wrap;
+    padding-top:10px;
+    padding-left:25px;
+    gap:10px;
+`;
+
+const TopRated = () => {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        const getMovies = async () => {
+            try {
+                const response = await axios.get(`https://api.themoviedb.org/3/movie/now_playing?language=ko-KR&page=1`,{
+                    headers:{
+                        Authorization:`Bearer ${import.meta.env.VITE_API_KEY}`
+                    }
+                });
+                setMovies(response.data.results);
+            } catch (error) {
+                console.error("Error fetching movies:", error);
+            }
+        };
+
+        getMovies();
+    }, []);
+
+    return (
+        <Container>
+            {movies.map((movie) => (
+                <MovieCard 
+                    key={movie.id} 
+                    title={movie.title}
+                    poster={movie.poster_path}
+                    release_date={movie.release_date}
+                />
+            ))}
+        </Container>
+    );
+};
+
+export default TopRated;
