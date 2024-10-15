5dfbd2f2169a0942d9366187db51f5b647f30b42
Sohyunnnn <thgus05061@naver.com>
2024-10-15 22:49:17 +0900

    FIX: env 및 url 재설정

--- 쏘_박소현/week4/mission/src/api/movies/getMovies.js
+++ 쏘_박소현/week4/mission/src/api/movies/getMovies.js
@@ -2,7 +2,7 @@ import axiosInstance from '..';
 
 export const getMovies = async (category) => {
   try {
-    const response = await axiosInstance.get(`${category}movie/?language=ko-KR&page=1`);
+    const response = await axiosInstance.get(`movie/${category}?language=ko-KR&page=1`);
     console.log(response.data);
     return response.data;
   } catch (error) {
