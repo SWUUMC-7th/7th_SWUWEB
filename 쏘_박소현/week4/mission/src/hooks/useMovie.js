654d0d8028e7d925edeb0b35efe501212254dc79
Sohyunnnn <thgus05061@naver.com>
2024-10-16 02:52:12 +0900

    FEAT: credit data 추가 및 UI 구현

--- 쏘_박소현/week4/mission/src/hooks/useMovie.js
+++ 쏘_박소현/week4/mission/src/hooks/useMovie.js
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import { getMovies } from "../api/movies/getMovies";
 import { getMovieDetail } from "../api/movies/getMovieDetail"; 
+import { getCredits } from "../api/movies/getCredits";
 
 const useMovie = (category, movieId) => {
   const [data, setData] = useState([]);
   const [movie, setMovie] = useState(null); 
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+  const [credits, setCredits] = useState({});
 
   useEffect(() => {
     const fetchData = async () => {
@@ -14,7 +16,10 @@ const useMovie = (category, movieId) => {
       try {
         if (movieId) { 
           const movieDetail = await getMovieDetail(movieId);
+          const creditsData = await getCredits(movieId);
+
           setMovie(movieDetail);
+          setCredits(creditsData);
         } else {
           const data = await getMovies(category);
           setData(data.results);
@@ -29,7 +34,7 @@ const useMovie = (category, movieId) => {
     fetchData();
   }, [category, movieId]); 
 
-  return { data, movie, isLoading, isError };
+  return { data, movie, credits, isLoading, isError };
 };
 
 export default useMovie;
--- 쏘_박소현/week4/mission/src/pages/DetailPage.jsx
+++ 쏘_박소현/week4/mission/src/pages/DetailPage.jsx
@@ -1,14 +1,18 @@
 import { useParams } from 'react-router-dom';
-import useMovie from '../hooks/useMovie'; 
+import useMovie from '../hooks/useMovie';
 import styled from 'styled-components';
 
 const Container = styled.div`
   margin: 50px;
   padding: 20px;
-  display: flex;
   gap: 30px;
   flex-direction: row;
   align-items: center;
+`;
+
+const DetailContainer = styled.div`
+  display: flex;
+  gap: 30px;
 `;
 
 const Poster = styled.img`
@@ -42,9 +46,51 @@ const Description = styled.p`
   text-align: start;
 `;
 
+const CreditsContainer = styled.div`
+  margin-top: 20px;
+  display: flex;
+  gap: 20px;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
+const CreditItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 10px 0;
+  flex: 0 0 calc(100% / 12);
+`;
+
+const CreditImage = styled.img`
+  width: 75px;
+  height: 75px;
+  border-radius: 50%;
+  margin-bottom: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+`;
+
+const DefaultImage = styled.div`
+  width: 75px;
+  height: 75px;
+  border-radius: 50%;
+  background-color: #d3d3d3;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+`;
+
+const CreditName = styled.p`
+  font-size: 0.9rem;
+  margin: 0;
+  text-align: center;
+`;
+
 const DetailPage = () => {
   const { movieId } = useParams();
-  const { movie, isLoading, isError } = useMovie(null, movieId);
+  const { movie, credits, isLoading, isError } = useMovie(null, movieId);
 
   if (isLoading) {
     return <p>Loading...</p>;
@@ -54,23 +100,48 @@ const DetailPage = () => {
     return <p>Error loading movie details.</p>;
   }
 
+  if (!movie) {
+    return <p>No movie found.</p>;
+  }
+
+  const director = credits.crew?.find(person => person.job === 'Director');
+  const cast = credits.cast || [];
+
   return (
-    <Container>
-      {movie && (
-        <>
-          <Poster src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-          <div>
-            <Title>{movie.title}</Title>
-            <InfoContainer>
-              <InfoItem>{movie.release_date}</InfoItem>
-              <InfoItem>{movie.vote_average}/10</InfoItem>
-              <InfoItem>{movie.runtime}분</InfoItem>
-            </InfoContainer>
-            <Description>{movie.overview}</Description>
-          </div>
-        </>
-      )}
-    </Container>
+    <>
+      <Container>
+        <DetailContainer>
+        <Poster src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+        <div>
+          <Title>{movie.title}</Title>
+          <InfoContainer>
+            <InfoItem>{movie.release_date}</InfoItem>
+            <InfoItem>{movie.vote_average}/10</InfoItem>
+            <InfoItem>{movie.runtime}분</InfoItem>
+          </InfoContainer>
+          <Description>{movie.overview}</Description>
+        </div>
+        </DetailContainer>
+        <div>
+        <h3>감독/출연</h3>
+      <CreditsContainer>
+        {[director, ...cast].map(person => (
+          <CreditItem key={person.id || person.name}> 
+            {person.profile_path ? (
+              <CreditImage src={`https://image.tmdb.org/t/p/w500${person.profile_path}`} alt={person.name} />
+            ) : (
+              <DefaultImage>
+                <span>{person.name.charAt(0)}</span>
+              </DefaultImage>
+            )}
+            <CreditName>{person.name}</CreditName>
+          </CreditItem>
+        ))}
+      </CreditsContainer>  
+      </div>
+      </Container>
+      
+    </>
   );
 };
 
