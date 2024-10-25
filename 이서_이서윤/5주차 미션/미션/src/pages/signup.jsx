a0e67c4081f8fee1b17b60db8b986508cb797145
seoyoon lee <leeseoyoon01@gmail.com>
2024-10-25 22:29:39 +0900

    FEAT: 회원가입 UI

--- 이서_이서윤/5주차 미션/미션/src/pages/signup.jsx
+++ 이서_이서윤/5주차 미션/미션/src/pages/signup.jsx
@@ -3,13 +3,65 @@ import styled from "styled-components";
 const Container=styled.div`
     background-color:black;
     width:calc(100vw - 200px);
-    *{color:white;}
+    display: flex;
+    justify-content: center; 
+    align-items: center;  
+    height:calc(100vh - 60px);
+    border-radius: 1px solid blue;
 `;
+const LogInBox=styled.div`
+    width: 500px;
+    height:700px;
+    text-align:center;
+`;
+const Title=styled.div`
+    font-size:25px;
+    font-weight:700;
+    color:white;
+    margin-bottom:20px;
+`;
+const Input=styled.input`
+    width:400px;
+    height:35px;
+    border-color:${(props)=>props.error ? 'red' : 'black'};
+    border-radius:10px;
+    margin-bottom:10px;
+`;
+const SignupBtn=styled.button`
+    width:410px;
+    height:45px;
+    background-color:${(props)=>props.disabled ? 'gray' : '#F2075D'};
+    color:white;
+`;
+// const Error=styled.div`
+//     color:red;
+//     font-size:15px;
+//     text-align:left;
+//     margin-left:50px;
+//     margin-top:-5px;
+//     margin-bottom:15px;
+// `;
 
 const SignUpPage = () => {
     return (
         <Container>
-            <h1>회원가입 페이지</h1>
+            <LogInBox>
+                <Title>회원가입</Title>
+                <Input 
+                    placeholder="이메일을 입력해주세요" 
+                />
+                {/* {emailCheck && <Error>{login.errors.email}</Error>} */}
+                <Input 
+                    type="password"
+                    placeholder="비밀번호를 입력해주세요" 
+                />
+                <Input 
+                    type="password"
+                    placeholder="비밀번호를 다시 입력해주세요" 
+                />
+                {/* {pwCheck && <Error>{login.errors.password}</Error>} */}
+                <SignupBtn>제출</SignupBtn>
+            </LogInBox>
         </Container>
     );
 };
