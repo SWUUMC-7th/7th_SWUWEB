17777a5f362883e28156ce078b67a459ccf41037
Minji Kim <minjikim2000@hotmail.com>
2024-10-30 01:59:41 +0900

    style: add login component

--- 레미_김민지/5주차 미션/실습1-LoginValidationCheck/src/pages/login.jsx
+++ 레미_김민지/5주차 미션/실습1-LoginValidationCheck/src/pages/login.jsx
@@ -1,9 +1,65 @@
+import styled from "styled-components";
+
 const Login = () => {
   return (
-    <>
-      <h1>로그인 페이지</h1>
-    </>
+    <Container>
+      <InfoWrapper>
+        <Title>로그인</Title>
+        <InputBox type="email" placeholder="이메일을 입력해주세요!" />
+        <InputBox type="password" placeholder="비밀번호를 입력해주세요!" />
+        <Button>로그인</Button>
+      </InfoWrapper>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 80%;
+`;
+
+const Title = styled.h1`
+  text-align: center;
+  margin: 28px 0;
+`;
+
+const InfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 30%;
+`;
+
+const InputBox = styled.input`
+  all: unset;
+  width: 100%;
+  height: 20px;
+  margin-bottom: 20px;
+  padding: 12px 16px;
+  border-radius: 12px;
+  background-color: white;
+  font-size: 14px;
+  color: black;
+`;
+
+const Button = styled.button`
+  all: unset;
+  width: 100%;
+  padding: 12px 16px;
+  border-radius: 12px;
+  background-color: #ff213b;
+  font-weight: bold;
+  text-align: center;
+  cursor: pointer;
+  transition: background-color 0.2s;
+  &:hover {
+    color: #ffffff96;
+    background-color: #ff213b8b; /* 호버 시 변경될 색상 */
+  }
+`;
 
 export default Login;
