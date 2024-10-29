ad93dbcdc7bbf154aebc4609c6b2828123bb99f9
Minji Kim <minjikim2000@hotmail.com>
2024-10-30 04:45:42 +0900

    feat: add validation on signup page

--- 레미_김민지/5주차 미션/미션-SignupPage/src/pages/signup.jsx
+++ 레미_김민지/5주차 미션/미션-SignupPage/src/pages/signup.jsx
@@ -1,20 +1,66 @@
 import styled from "styled-components";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const schema = yup.object({
+  email: yup
+    .string()
+    .email("유효한 이메일 형식이 아닙니다.")
+    .required("이메일은 필수 입력요소입니다."),
+  password: yup
+    .string()
+    .min(8, "비밀번호는 8자 이상이어야 합니다.")
+    .max(16, "비밀번호는 16자 이하여야 합니다.")
+    .required("비밀번호는 필수 입력요소입니다."),
+  passwordCheck: yup
+    .string()
+    .oneOf([yup.ref("password")], "비밀번호가 일치하지 않습니다.")
+    .required("비밀번호 확인은 필수 입력요소입니다."),
+});
 
 const Signup = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data) => {
+    console.log("회원가입 데이터:", data);
+  };
+
   return (
     <Container>
       <InfoWrapper>
         <Title>회원가입</Title>
-        <InputWrapper>
-          <InputBox type="email" placeholder="이메일을 입력해주세요!" />
-        </InputWrapper>
-        <InputWrapper>
-          <InputBox type="password" placeholder="비밀번호를 입력해주세요!" />
-        </InputWrapper>
-        <InputWrapper>
-          <InputBox type="password" placeholder="비밀번호를 다시 입력해주세요!" />
-        </InputWrapper>
-        <Button>가입하기</Button>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <InputWrapper>
+            <InputBox type="email" placeholder="이메일을 입력해주세요!" {...register("email")} />
+            {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
+          </InputWrapper>
+          <InputWrapper>
+            <InputBox
+              type="password"
+              placeholder="비밀번호를 입력해주세요!"
+              {...register("password")}
+            />
+            {errors.password && <ErrorMessage>{errors.password.message}</ErrorMessage>}
+          </InputWrapper>
+          <InputWrapper>
+            <InputBox
+              type="password"
+              placeholder="비밀번호를 다시 입력해주세요!"
+              {...register("passwordCheck")}
+            />
+            {errors.passwordCheck && <ErrorMessage>{errors.passwordCheck.message}</ErrorMessage>}
+          </InputWrapper>
+          <Button type="submit" disabled={Object.keys(errors).length > 0}>
+            가입하기
+          </Button>
+        </Form>
       </InfoWrapper>
     </Container>
   );
@@ -39,6 +85,10 @@ const InfoWrapper = styled.div`
   align-items: center;
   justify-content: center;
   width: 40%;
+`;
+
+const Form = styled.form`
+  width: 100%;
 `;
 
 const InputWrapper = styled.div`
