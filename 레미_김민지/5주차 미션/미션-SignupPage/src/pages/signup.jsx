8be452bc7f84bead59c135b83404d3a47ee617ab
Minji Kim <minjikim2000@hotmail.com>
2024-10-30 04:58:06 +0900

    fix: disable button when input is blank

--- 레미_김민지/5주차 미션/미션-SignupPage/src/pages/signup.jsx
+++ 레미_김민지/5주차 미션/미션-SignupPage/src/pages/signup.jsx
@@ -4,28 +4,26 @@ import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 
 const schema = yup.object({
-  email: yup
-    .string()
-    .email("유효한 이메일 형식이 아닙니다.")
-    .required("이메일은 필수 입력요소입니다."),
+  email: yup.string().email("유효한 이메일 형식이 아닙니다.").required("이메일을 입력 해주세요."),
   password: yup
     .string()
-    .min(8, "비밀번호는 8자 이상이어야 합니다.")
-    .max(16, "비밀번호는 16자 이하여야 합니다.")
-    .required("비밀번호는 필수 입력요소입니다."),
+    .min(8, "비밀번호는 8자 이상 입력 해주세요.")
+    .max(16, "비밀번호는 16자 이하 입력 해주세요.")
+    .required("비밀번호를 입력 해주세요."),
   passwordCheck: yup
     .string()
     .oneOf([yup.ref("password")], "비밀번호가 일치하지 않습니다.")
-    .required("비밀번호 확인은 필수 입력요소입니다."),
+    .required("비밀번호 다시 확인해주세요."),
 });
 
 const Signup = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
   } = useForm({
     resolver: yupResolver(schema),
+    mode: "onChange",
   });
 
   const onSubmit = (data) => {
@@ -57,7 +55,7 @@ const Signup = () => {
             />
             {errors.passwordCheck && <ErrorMessage>{errors.passwordCheck.message}</ErrorMessage>}
           </InputWrapper>
-          <Button type="submit" disabled={Object.keys(errors).length > 0}>
+          <Button type="submit" disabled={!isValid}>
             가입하기
           </Button>
         </Form>
