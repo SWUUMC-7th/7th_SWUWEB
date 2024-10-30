84bd921b90ad2cf6f74b5fb3bcf7aade78d55290
seoyoon lee <leeseoyoon01@gmail.com>
2024-10-30 22:14:39 +0900

    FEAT: 이름, 성별, 생년월일 추가

--- 이서_이서윤/5주차 미션/미션/src/pages/signup.jsx
+++ 이서_이서윤/5주차 미션/미션/src/pages/signup.jsx
@@ -3,7 +3,6 @@ import {useForm} from 'react-hook-form'
 import * as yup from 'yup'
 import {yupResolver} from '@hookform/resolvers/yup'
 
-
 const Container=styled.div`
     background-color:black;
     width:calc(100vw - 200px);
@@ -13,7 +12,7 @@ const Container=styled.div`
     height:calc(100vh - 60px);
     border-radius: 1px solid blue;
 `;
-const LogInBox=styled.div`
+const SignupBox=styled.div` 
     width: 500px;
     height:700px;
     text-align:center;
@@ -45,9 +44,33 @@ const Error=styled.div`
     margin-top:-5px;
     margin-bottom:15px;
 `;
-
+const GenderBirth=styled.div`
+    margin: 0 auto;
+    display:flex;
+    justify-content:space-between;
+    color:#F2075D;
+    width:410px;
+    height:45px;
+    margin-top:-10px;
+    margin-bottom:10px; 
+    line-height:45px;
+    div{
+        margin-left:5px;
+        margin-right:10px;}
+`;
+const Gender=styled.div`
+    display:flex;
+    width:300px;
+    >label{color:white !important ;}
+`
+const Birth=styled.div`
+    display:flex;
+    width:300px;
+    >input{width:100px !important ;}
+`;
 const SignUpPage = () => {
     const schema = yup.object().shape({
+        name:yup.string().required('이름을 입력해주세요.'),
         email:yup.string().email('유효한 이메일이 아닙니다.').required('이메일을 입력해주세요.'),
         password:yup.string().min(8,'비밀번호는 8자 이상이어야 합니다.').max(16,'비밀번호는 16자 이하여야 합니다.')
         .required('비밀번호를 입력해주세요.'),
@@ -55,8 +78,9 @@ const SignUpPage = () => {
         .oneOf([yup.ref('password'), null], '비밀번호가 일치하지 않습니다.')
         .required('비밀번호 확인은 필수입니다.')
     })
-    const {register, handleSubmit, formState: {errors}} = useForm({
-        resolver:yupResolver(schema)
+    const {register, handleSubmit, formState: {errors, isValid}} = useForm({
+        resolver:yupResolver(schema),
+        mode: 'onChange' 
     });
     const onSubmit = (data) => {
         console.log('폼 데이터 제출');
@@ -64,29 +88,58 @@ const SignUpPage = () => {
     }
     return (
         <Container>
-            <LogInBox>
+            <SignupBox>
                 <Title>회원가입</Title>
-                <form  onSubmit={handleSubmit(onSubmit)}>
+                <form  onSubmit={handleSubmit(onSubmit)}> 
+                    <Input 
+                        placeholder="이름을 입력해주세요" 
+                        error={errors.name}
+                        {...register("name")}
+                    />
+                    <Error>{errors.name?.message}</Error>
+                    <GenderBirth>
+                        <Gender>
+                            <div>성별</div>
+                            <label>
+                                <input type="radio" label="gender" value="male"/>남
+                            </label>
+                            <label>
+                            <input type="radio" label="gender" value="female"/>여
+                            </label>
+                        </Gender>
+                        <Birth>
+                            <div>생년월일</div>
+                            <Input type="date"/>
+                        </Birth>
+                    </GenderBirth>
                     <Input 
                         placeholder="이메일을 입력해주세요" 
+                        error={errors.email}
                         {...register("email")}
                     />
                     <Error>{errors.email?.message}</Error>
                     <Input 
                         type="password"
                         placeholder="비밀번호를 입력해주세요" 
+                        error={errors.password}
                         {...register("password")}
                     />
                     <Error>{errors.password?.message}</Error>
                     <Input 
                         type="password"
-                        placeholder="비밀번호를 다시 입력해주세요" 
+                        placeholder="비밀번호를 다시 입력해주세요"
+                        error={errors.passwordCheck}
                         {...register("passwordCheck")}
                     /> 
                     <Error>{errors.passwordCheck?.message}</Error>
-                    <SignupBtn type="submit">제출</SignupBtn>
+                    <SignupBtn 
+                        type="submit"
+                        disabled={!isValid}
+                    >
+                        제출
+                    </SignupBtn>
                 </form>
-            </LogInBox>
+            </SignupBox>
         </Container>
     );
 };
