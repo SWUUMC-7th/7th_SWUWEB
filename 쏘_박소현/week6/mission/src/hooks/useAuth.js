6fb4dfb0af4953334c4d6f8973c94ee021da626e
Sohyunnnn <thgus05061@naver.com>
2024-11-06 02:58:49 +0900

    REFACTOR: 코드 리팩토링 진행

--- 쏘_박소현/week6/mission/src/hooks/useAuth.js
+++ 쏘_박소현/week6/mission/src/hooks/useAuth.js
@@ -1,13 +1,7 @@
-import { useState } from 'react';
 import { postSignUp } from '../api/auth/postSignUp';
 import { postSignIn } from '../api/auth/postSignIn';
 
 const useAuth = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordCheck, setPasswordCheck] = useState('');
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
 
 
   const handleSignUp = async (data) => {
@@ -17,34 +11,20 @@ const useAuth = () => {
       return response;
     } catch (err) {
       console.error('회원가입 요청 실패:', err);
-      setError(err.response?.data?.message || '회원가입 실패');
-    } finally {
-      setLoading(false);
-    }
+    } 
   };
 
-  const handleSignIn = async () => {
+  const handleSignIn = async (data) => {
     try {
-      const data = { email, password };
       const response = await postSignIn(data);
       console.log('로그인 성공:', response);
       return response;
     } catch (err) {
       console.error('로그인 요청 실패:', err);
-      setError(err.response?.data?.message || '로그인 실패');
-    } finally {
-      setLoading(false);
     }
   };
+  
   return {
-    email,
-    setEmail,
-    password,
-    setPassword,
-    passwordCheck,
-    setPasswordCheck,
-    error,
-    loading,
     handleSignUp,
     handleSignIn,
   };
