1be19d2307775b043a7205985a18d82b5dff3b1c
Sohyunnnn <thgus05061@naver.com>
2024-11-06 03:28:57 +0900

    FEAT: 토큰 저장

--- 쏘_박소현/week6/mission/src/hooks/useAuth.js
+++ 쏘_박소현/week6/mission/src/hooks/useAuth.js
@@ -1,29 +1,31 @@
-import { postSignUp } from '../api/auth/postSignUp';
-import { postSignIn } from '../api/auth/postSignIn';
+import { postSignUp } from "../api/auth/postSignUp";
+import { postSignIn } from "../api/auth/postSignIn";
 
 const useAuth = () => {
-
-
   const handleSignUp = async (data) => {
     try {
       const response = await postSignUp(data);
-      console.log('회원가입 성공:', response);
+      console.log("회원가입 성공:", response);
       return response;
     } catch (err) {
-      console.error('회원가입 요청 실패:', err);
-    } 
+      console.error("회원가입 요청 실패:", err);
+    }
   };
 
   const handleSignIn = async (data) => {
     try {
       const response = await postSignIn(data);
-      console.log('로그인 성공:', response);
+      console.log("로그인 성공:", response);
+
+      localStorage.setItem("refreshToken", response.refreshToken);
+      localStorage.setItem("accessToken", response.accessToken);
+
       return response;
     } catch (err) {
-      console.error('로그인 요청 실패:', err);
+      console.error("로그인 요청 실패:", err);
     }
   };
-  
+
   return {
     handleSignUp,
     handleSignIn,
