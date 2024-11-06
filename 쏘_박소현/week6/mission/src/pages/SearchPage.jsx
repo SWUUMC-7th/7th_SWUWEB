c8e90dfb064ec52c075721134d4cc81aaff2fd34
Sohyunnnn <thgus05061@naver.com>
2024-11-06 11:57:12 +0900

    FEAT: 검색창 구현

--- 쏘_박소현/week6/mission/src/pages/SearchPage.jsx
+++ 쏘_박소현/week6/mission/src/pages/SearchPage.jsx
@@ -1,8 +1,33 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+`;
+
+const InputCotainer = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+const Input = styled.input`
+  width: 500px;
+`;
+
+const Button = styled.button`
+  background-color: #ff3557; 
+`;
 
 const SearchPage = () => {
   return (
-    <div>SearchPage</div>
-  )
-}
+    <Container>
+      <InputCotainer>
+      <Input />
+      <Button>검색</Button>
+      </InputCotainer>
+    </Container>
+  );
+};
 
-export default SearchPage
+export default SearchPage;
