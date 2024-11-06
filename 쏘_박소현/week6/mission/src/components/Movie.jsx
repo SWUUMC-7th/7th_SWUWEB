f85177129456a9ad0fcaae68639b549bb5811879
Sohyunnnn <thgus05061@naver.com>
2024-11-06 18:25:57 +0900

    FEAT: 컴포넌트 분리

--- 쏘_박소현/week6/mission/src/components/Movie.jsx
+++ 쏘_박소현/week6/mission/src/components/Movie.jsx
@@ -1,75 +1,39 @@
-import { useParams , useNavigate } from 'react-router-dom';
-import useMovie from '../hooks/useMovie';
-import styled from 'styled-components';
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
-const baseUrl = 'https://image.tmdb.org/t/p/w500';
-
-const MovieContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(160px, 1fr)); 
-  gap: 10px;
-  margin: 0 auto;
-  max-width: 1300px; 
-`;
+const baseUrl = "https://image.tmdb.org/t/p/w500";
 
 const MovieItem = styled.div`
   position: relative;
   overflow: hidden;
+  cursor: pointer;
 
   &:hover img {
-    filter: brightness(20%); 
+    filter: brightness(20%);
   }
 `;
 
 const MovieImage = styled.img`
   width: 100%;
   border-radius: 5px;
-  transition: filter 0.3s ease; 
+  transition: filter 0.3s ease;
 `;
 
-const LoadingMessage = styled.p`
-  text-align: center;
-  font-size: 18px;
-  font-weight: bold;
-`;
-
-const ErrorMessage = styled.p`
-  text-align: center;
-  color: red;
-  font-size: 18px;
-  font-weight: bold;
-`;
-
-const Movie = () => {
+const Movie = ({ movie }) => {
   const navigate = useNavigate();
-  const { category } = useParams();
-  const { data: movies, isLoading, isError } = useMovie(category);
 
-  const handleMovieClick = (movieId) => {
-    navigate(`/movies/${movieId}`);
+  const handleMovieClick = () => {
+    navigate(`/movies/${movie.id}`); 
   };
 
-  if (isLoading) {
-    return <LoadingMessage>Loading movies...</LoadingMessage>;
-  }
-
-  if (isError) {
-    return <ErrorMessage>Failed to load movies. Please try again later.</ErrorMessage>;
-  }
-
   return (
-    <MovieContainer>
-      {movies.map(movie => (
-        <MovieItem key={movie.id} onClick={() => handleMovieClick(movie.id)}>
-          <MovieImage
-            src={`${baseUrl}${movie.poster_path}`}
-            alt={movie.title}
-          />
-          <p>{movie.title}</p>
-          <p>{movie.release_date}</p>
-        </MovieItem>
-      ))}
-    </MovieContainer>
+    <MovieItem onClick={handleMovieClick}>
+      <MovieImage src={`${baseUrl}${movie.poster_path}`} alt={movie.title} />
+      <p>{movie.title}</p>
+      <p>{movie.release_date}</p>
+      <p>{movie.vote_average} / 10</p>
+      <p>{movie.overview}</p>
+    </MovieItem>
   );
 };
 
