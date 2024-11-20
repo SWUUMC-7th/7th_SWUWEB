69e1a8ba331a8872690404e057da5611063f4f13
Sohyunnnn <thgus05061@naver.com>
2024-11-20 23:22:48 +0900

    FEAT: 버튼 disabled 로직 추가

--- 쏘_박소현/week8/TodoList/src/components/Button.jsx
+++ 쏘_박소현/week8/TodoList/src/components/Button.jsx
@@ -3,21 +3,23 @@ import styled from "styled-components";
 const StyledButton = styled.button`
   width: 100px;
   height: 36px;
-  background-color: rgb(99, 151, 255);
+  background-color: ${(props) =>
+    props.disabled ? "rgb(187, 187, 187)" : "rgb(99, 151, 255)"};
   border: none;
   border-radius: 4px;
-  color: white;
-  cursor: pointer;
+  color: ${(props) => (props.disabled ? "rgba(255, 255, 255, 0.5)" : "white")};
+  cursor: ${(props) => (props.disabled ? "not-allowed" : "pointer")};
   transition: background-color 0.3s;
 
   &:hover {
-    background-color: rgb(77, 131, 240);
+    background-color: ${(props) =>
+      props.disabled ? "rgb(187, 187, 187)" : "rgb(77, 131, 240)"};
   }
 `;
 
-const Button = ({ onClick, children, type = "button" }) => {
+const Button = ({ onClick, children, type = "button", disabled = false }) => {
   return (
-    <StyledButton onClick={onClick} type={type}>
+    <StyledButton onClick={onClick} type={type} disabled={disabled}>
       {children}
     </StyledButton>
   );
