6432840b93e82d1f0c542ae55d9bf47a1444a562
seoyoon lee <leeseoyoon01@gmail.com>
2024-11-23 21:05:26 +0900

    FEAT: 실습 redux-toolkit 이용한 todoList

---
+++ 이서_이서윤/9주차/실습/src/components/Button.jsx
@@ -0,0 +1,22 @@
+import '../App.css'
+import PropTypes from 'prop-types';
+
+function Button(props){
+    const {onClick, children,  id, className}=props;
+    return(
+        <button 
+            onClick={onClick} 
+            id={id}
+            className={className}
+        >
+            {children}
+        </button>
+    )
+}
+Button.propTypes={
+    onClick:PropTypes.func,
+    children:PropTypes.string,
+    id:PropTypes.string,
+    className:PropTypes.string
+}
+export default Button;
