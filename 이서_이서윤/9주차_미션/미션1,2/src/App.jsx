14382fa19e093cb8d5c2684cf7a901e449768a8c
seoyoon lee <leeseoyoon01@gmail.com>
2024-11-23 14:53:25 +0900

    FEAT: 미션1

---
+++ 이서_이서윤/9주차_미션/미션1,2/src/App.jsx
@@ -0,0 +1,24 @@
+import Navbar from "./components/Navbar"
+import CartContainer from "./components/CartContainer"
+import { useDispatch, useSelector } from "react-redux"
+import { useEffect } from "react";
+import { calculateTotal } from "./features/cart/cartSlice";
+
+function App() {
+  const dispatch=useDispatch();
+  const {cartItems}=useSelector((store)=>store.cart)
+  useEffect(()=>{
+    dispatch(calculateTotal());
+  },[cartItems,dispatch])
+
+  return (
+    <>
+      <header>
+        <Navbar/>
+      </header>
+      <CartContainer/>
+    </>
+  )
+}
+
+export default App
