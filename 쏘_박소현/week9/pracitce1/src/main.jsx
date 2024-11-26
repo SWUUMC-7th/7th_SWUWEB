468fd698487e264f5e57c8fc6ce946c49e2336ae
Sohyunnnn <thgus05061@naver.com>
2024-11-27 00:12:01 +0900

    FEAT: 미션1 todo 구현

--- 쏘_박소현/week9/pracitce1/src/main.jsx
+++ 쏘_박소현/week9/pracitce1/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
     <App />
-  </StrictMode>,
-)
+  </Provider>
+);
+
+reportWebVitals();
