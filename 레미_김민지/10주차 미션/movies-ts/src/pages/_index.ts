464e604eab5c466560d0c81bdeeb2d6c91c20a49
Minji Kim <minjikim2000@hotmail.com>
2024-12-04 06:23:35 +0900

    feat: add NowPlayingMovies 페이지

--- 레미_김민지/10주차 미션/movies-ts/src/pages/_index.ts
+++ 레미_김민지/10주차 미션/movies-ts/src/pages/_index.ts
@@ -1,4 +1,5 @@
 export { default as Home } from "./Home";
 export { default as Movies } from "./Movies";
 export { default as NotFound } from "./NotFound";
+export { default as NowPlayingMovies } from "./NowPlayingMovies";
 export { default as Search } from "./Search";
