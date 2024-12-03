53b845b6307b15af059b8f63e2bd522dc39bf455
Sohyunnnn <thgus05061@naver.com>
2024-12-04 03:58:07 +0900

    STYLE: 스타일 수정

--- 쏘_박소현/week10/mission/src/routes/router.tsx
+++ 쏘_박소현/week10/mission/src/routes/router.tsx
@@ -1,4 +1,4 @@
-import {createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import NotFound from "../pages/NotFound";
 import MoviesPage from "../pages/MoviesPage";
@@ -9,46 +9,42 @@ import SearchPage from "../pages/SearchPage";
 import CategoryPage from "../pages/CategoryPage";
 import DetailPage from "../pages/DetailPage";
 
-
 const router = createBrowserRouter([
   {
-      path: '/',
-      element: <RootLayout/>,
-      errorElement: <NotFound/>,
-      children: [
-        {
-            index: true,
-            element: <HomePage/>
-        },
-        {
-          path: 'movies/categories/:category',
-            element: <MoviesPage/>
-        },
-        {
-          path: 'movies/:movieId',
-            element: <DetailPage/>
-        },
-        {
-          path: 'sign-in',
-            element: <SignInPage/>
-        },
-        {
-          path: 'sign-up',
-            element: <SignUpPage/>
-        },
-        {
-          path: 'search',
-            element: <SearchPage/>
-        },
-        {
-          path: 'movies/categories',
-            element: <CategoryPage/>
-        },
-    ]
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "movies/categories/:category",
+        element: <MoviesPage />,
+      },
+      {
+        path: "movies/:movieId",
+        element: <DetailPage />,
+      },
+      {
+        path: "sign-in",
+        element: <SignInPage />,
+      },
+      {
+        path: "sign-up",
+        element: <SignUpPage />,
+      },
+      {
+        path: "search",
+        element: <SearchPage />,
+      },
+      {
+        path: "movies/categories",
+        element: <CategoryPage />,
+      },
+    ],
   },
+]);
 
-  ])
-
-
-
-export default router
+export default router;
