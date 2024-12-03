f53b33fae6f239ecbeda6d13a7349b76e485f8b9
Minji Kim <minjikim2000@hotmail.com>
2024-12-04 04:40:48 +0900

    feat: tanstack-query 초기 세팅 추가

--- 레미_김민지/10주차 미션/movies-ts/src/App.tsx
+++ 레미_김민지/10주차 미션/movies-ts/src/App.tsx
@@ -1,8 +1,17 @@
 import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import router from "./routes/router";
 
+const queryClient = new QueryClient();
+
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
