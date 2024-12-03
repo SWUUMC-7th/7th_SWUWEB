ddcf90ae53e35ffd8a2e35a290948cd05283777b
Minji Kim <minjikim2000@hotmail.com>
2024-12-04 03:02:50 +0900

    tests: 테스트를 위한 임시 주석처리

--- 레미_김민지/10주차 미션/movies-ts/src/routes/router.tsx
+++ 레미_김민지/10주차 미션/movies-ts/src/routes/router.tsx
@@ -1,63 +1,64 @@
 import { createBrowserRouter } from "react-router-dom";
 import RoutePaths from "./RoutePaths";
+import RootLayout from "../layouts/RootLayout";
 
 const router = createBrowserRouter([
   {
     path: RoutePaths.HOME,
     element: <RootLayout />,
-    errorElement: <NotFound />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: RoutePaths.MOVIES,
-        element: <Movies />, // Category는 MoviesPage 안에서 렌더링됨
-        children: [
-          {
-            path: RoutePaths.MOVIES_NOW_PLAYING,
-            element: <NowPlayingMovies />,
-          },
-          {
-            path: RoutePaths.MOVIES_POPULAR,
-            element: <PopularMovies />,
-          },
-          {
-            path: RoutePaths.MOVIES_TOP_RATED,
-            element: <TopRatedMovies />,
-          },
-          {
-            path: RoutePaths.MOVIES_UPCOMING,
-            element: <UpComingMovies />,
-          },
-          {
-            path: RoutePaths.MOVIE_DETAIL,
-            element: <MovieDetail />,
-          },
-        ],
-      },
-      {
-        path: RoutePaths.ACTORS,
-        element: <Actors />,
-      },
-      {
-        path: RoutePaths.ACTOR_DETAIL,
-        element: <ActorDetail />,
-      },
-      {
-        path: RoutePaths.LOGIN,
-        element: <Login />,
-      },
-      {
-        path: RoutePaths.SIGNUP,
-        element: <SignUp />,
-      },
-      {
-        path: RoutePaths.SEARCH,
-        element: <Search />,
-      },
-    ],
+    //errorElement: <NotFound />,
+    // children: [
+    //   {
+    //     index: true,
+    //     element: <Home />,
+    //   },
+    //   {
+    //     path: RoutePaths.MOVIES,
+    //     element: <Movies />, // Category는 MoviesPage 안에서 렌더링됨
+    //     children: [
+    //       {
+    //         path: RoutePaths.MOVIES_NOW_PLAYING,
+    //         element: <NowPlayingMovies />,
+    //       },
+    //       {
+    //         path: RoutePaths.MOVIES_POPULAR,
+    //         element: <PopularMovies />,
+    //       },
+    //       {
+    //         path: RoutePaths.MOVIES_TOP_RATED,
+    //         element: <TopRatedMovies />,
+    //       },
+    //       {
+    //         path: RoutePaths.MOVIES_UPCOMING,
+    //         element: <UpComingMovies />,
+    //       },
+    //       {
+    //         path: RoutePaths.MOVIE_DETAIL,
+    //         element: <MovieDetail />,
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     path: RoutePaths.ACTORS,
+    //     element: <Actors />,
+    //   },
+    //   {
+    //     path: RoutePaths.ACTOR_DETAIL,
+    //     element: <ActorDetail />,
+    //   },
+    //   {
+    //     path: RoutePaths.LOGIN,
+    //     element: <Login />,
+    //   },
+    //   {
+    //     path: RoutePaths.SIGNUP,
+    //     element: <SignUp />,
+    //   },
+    //   {
+    //     path: RoutePaths.SEARCH,
+    //     element: <Search />,
+    //   },
+    // ],
   },
 ]);
 
