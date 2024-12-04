7d2ba7f416b96f05602e7ae0731a7e6ed4fbf7a3
Sohyunnnn <thgus05061@naver.com>
2024-12-04 11:41:56 +0900

    TYPE: api 타입 정의

---
+++ 쏘_박소현/week10/mission/src/api/movies/getSearch.ts
@@ -0,0 +1,14 @@
+import { axiosInstance } from "..";
+
+export const getSearch = async (search: string) => {
+  try {
+    const response = await axiosInstance.get(
+      `search/movie?query=${search}&language=ko-KR`
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+};
