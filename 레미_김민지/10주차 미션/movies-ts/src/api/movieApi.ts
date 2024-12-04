9f703bd11720b48d0a3aa3f9c6940e126745d622
Minji Kim <minjikim2000@hotmail.com>
2024-12-04 10:37:35 +0900

    feat: add 영화  상세 정보 API 추가

--- 레미_김민지/10주차 미션/movies-ts/src/api/movieApi.ts
+++ 레미_김민지/10주차 미션/movies-ts/src/api/movieApi.ts
@@ -1,4 +1,5 @@
 import { axiosInstance } from "./axiosInstance";
+import { MovieCredits, MovieDetails } from "./types";
 
 interface GetMoviesParams {
   category: string;
@@ -7,5 +8,15 @@ interface GetMoviesParams {
 
 export const getMovies = async ({ category, pageParam }: GetMoviesParams) => {
   const { data } = await axiosInstance.get(`/movie/${category}?language=ko&page=${pageParam}`);
+  return data;
+};
+
+export const getMovieDetails = async (movieId: string): Promise<MovieDetails> => {
+  const { data } = await axiosInstance.get(`/movie/${movieId}?language=ko`);
+  return data;
+};
+
+export const getMovieCredits = async (movieId: string): Promise<MovieCredits> => {
+  const { data } = await axiosInstance.get(`/movie/${movieId}/credits?language=ko`);
   return data;
 };
